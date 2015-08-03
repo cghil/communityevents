4d87e6fcd2a33e98534d5c072dbfc63e3288658f
cghil <cghilarducci@gmail.com>
2015-08-03 14:18:06 -0700

    read event route done, adding the add event (view finished, post request is not)

--- views/events/index.js
+++ views/events/index.js
@@ -34,3 +34,31 @@ exports.find = function(req, res, next){
     }
   });
 };
+
+exports.read = function(req, res, next){
+	req.app.db.models.Event.findById(req.params.id).exec(function(err, event){
+		if(err){
+			return next(err)
+		}
+
+		if (req.xhr){
+			res.send(event)
+		} else {
+			res.render('events/details', { event: event })
+		}
+	})
+};
+
+exports.add = function(req, res){
+  if (!req.isAuthenticated()) {
+    req.flash("error", "You are not logged in");
+      res.location('/events');
+      res.redirect("/events");
+  }
+  res.render('events/add');
+};
+
+// exports.create = function(req, res, next){
+//   var workflow = 
+// }
+
